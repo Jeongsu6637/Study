d34ef4967368bcb32af46518bbc28121a6d82aff
Sudongk <aa88sd@naver.com>
2023-08-01 11:26:07 +0900

    init

---
+++ src/test/java/com/stock/market/StockMarketInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
